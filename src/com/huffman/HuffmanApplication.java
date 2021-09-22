1faa16cb219ae9aab5e4adf3ae6ee2fa756b39d9
junyoung.park <pkhd5454@naver.com>
2021-09-22 23:50:18 +0900

    huffman 1단계> 파일을 읽고 run length symbol 추출

--- src/com/huffman/HuffmanApplication.java
+++ src/com/huffman/HuffmanApplication.java
@@ -1,7 +1,48 @@
 package com.huffman;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.List;
+
 public class HuffmanApplication {
+  private List<Run> runs = new ArrayList<>();
+
   public static void main(String[] args) {
-    // write your code here
+    HuffmanApplication app = new HuffmanApplication();
+    RandomAccessFile fIn;
+    try {
+      fIn = new RandomAccessFile("test.jpg", "r");
+      app.collectRuns(fIn);
+      fIn.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /** 데이터 파일 fIn에 등장하는 모든 run들과 각각의 등장횟수를 count하여 ArrayList runs에 저장한다. */
+  private void collectRuns(RandomAccessFile fIn) throws IOException {
+    Byte prev = null;
+    Byte curr;
+    int length = 1;
+    while (fIn.getFilePointer() != fIn.length()) {
+      curr = fIn.readByte();
+      if (prev != null) {
+        if (prev == curr) {
+          length++;
+        } else {
+          Run run = new Run(prev, length);
+          if (runs.contains(run)) {
+            runs.get(runs.indexOf(run)).freq++;
+          } else {
+            runs.add(run);
+          }
+          length = 1;
+        }
+      }
+      prev = curr;
+    }
+    fIn.seek(0);
+    System.out.println(runs);
   }
 }
---
+++ src/com/huffman/Run.java
@@ -0,0 +1,39 @@
+package com.huffman;
+
+import java.util.Objects;
+
+public class Run {
+  public byte symbol;
+  public int runLen;
+  public int freq;
+
+  public Run() {}
+
+  public Run(byte symbol, int runLen) {
+    this.symbol = symbol;
+    this.runLen = runLen;
+    freq = 1;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Run run = (Run) o;
+    return symbol == run.symbol && runLen == run.runLen;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(symbol, runLen);
+  }
+
+  @Override
+  public String toString() {
+    return "\nRun{" + "symbol=" + symbol + ", runLen=" + runLen + ", freq=" + freq + '}';
+  }
+}
